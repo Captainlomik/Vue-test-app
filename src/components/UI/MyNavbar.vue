d61b7908337f731f566a973f011836168d27ff18
captainlomik <nastiy248@mail.ru>
2023-05-17 15:49:54 +0300

    add router

---
+++ src/components/UI/MyNavbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="navbar">
+        <div>Logo</div>
+        <div class="navbar__bts">
+            <my-button>Посты</my-button>
+            <my-button>О нас</my-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'my-navbar'
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+    height: 50px;
+    background-color: lightgrey;
+    box-shadow: 2px 2px 4px gray;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    &__bts {
+        width: 50%;
+    }
+
+    .btn{
+        margin-right: 20px;
+    }
+}
+</style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -3,11 +3,13 @@ import MyInput from "@/components/UI/MyInput"
 import MyDialog from "@/components/UI/MyDialog"
 import MYSelect from "@/components/UI/MySelect"
 import MyPagination from "@/components/UI/MyPagination"
+import MyNavbar from "@/components/UI/MyNavbar"
 
 export default [
     MyButton, 
     MyInput, 
     MyDialog, 
     MYSelect, 
-    MyPagination
+    MyPagination, 
+    MyNavbar
 ]
