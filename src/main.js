4f1f8decde2f9192264fd38de39165bd3fd4650d
captainlomik <nastiy248@mail.ru>
2023-05-17 15:50:08 +0300

    add pages

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import components from '@/components/UI/index'
+import router from "@/router/router";
 
 const app = createApp(App)
 components.forEach(el => {
     app.component(el.name, el)
 })
 
+
+app.use(router)
 app.mount('#app')
---
+++ src/pages/PostPage.vue
@@ -0,0 +1,134 @@
+<template >
+    <div>
+      <h1>Страница с постами</h1>
+      <my-input v-model="searchQuery" placeholder="Поиск...." />
+      <div class="app__btns">
+        <my-button class="dialog-btn" @click="showDialog">Создать пост</my-button>
+        <my-select v-model="selectedSort" :options="sortOptions"></my-select>
+        <my-dialog v-model:show="dialogVisible">
+          <post-form @create="createPost"></post-form>
+        </my-dialog>
+      </div>
+  
+      <post-list v-if="!isPostLoading" :posts="searchPosts" @remove="removePost"></post-list>
+      <div v-else>
+        <p>Идет загрузка</p>
+      </div>
+      <div ref="observer" class="observer"></div>
+      <!-- <my-pagination @my-event="page" :pages="totalPages" :pageNumber="page"></my-pagination> -->
+    </div>
+  </template>
+  
+  <script>
+  import PostForm from "@/components/PostForm"
+  import PostList from '@/components/PostList.vue'
+  import axios from 'axios'
+  
+  export default {
+    components: {
+      PostList, PostForm
+    },
+    data() {
+      return {
+        posts: [],
+        dialogVisible: false,
+        isPostLoading: false,
+        selectedSort: '',
+        searchQuery: '',
+        page: 1,
+        limit: 10,
+        totalPages: 0,
+        sortOptions: [
+          { value: 'title', name: 'По названию' },
+          { value: 'description', name: 'По описанию' },
+        ]
+      }
+    },
+    methods: {
+      createPost(post) {
+        this.posts.push(post)
+        this.dialogVisible = false
+      },
+      removePost(post) {
+        this.posts = this.posts.filter(p => p.id !== post.id)
+      },
+      showDialog() {
+        this.dialogVisible = true
+      },
+      async fetchPosts() {
+        try {
+          this.isPostLoading = true
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+            params: {
+              _page: this.page,
+              _limit: this.limit
+            }
+          })
+          this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+          this.posts = response.data
+        }
+        catch (e) {
+          console.error('Error!')
+        }
+        finally {
+          this.isPostLoading = false
+        }
+      },
+      async loadMorePOst() {
+        try {
+          this.page += 1
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+            params: {
+              _page: this.page,
+              _limit: this.limit
+            }
+          })
+          this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+          this.posts = [...this.posts, ...response.data]
+        }
+        catch (e) {
+          console.error('Error!')
+        }
+      }
+    },
+    mounted() {
+      this.fetchPosts()
+      const options = {
+        rootMargin: '0px',
+        threshold: 1.0
+      }
+      const callback = (entries) => {
+        if (entries[0].isIntersecting && this.page < this.totalPages) {
+          this.loadMorePOst()
+        }
+      }
+      const observer = new IntersectionObserver(callback, options)
+      observer.observe(this.$refs.observer)
+    },
+    computed: {
+      sortedPost() {
+        return [...this.posts].sort((post1, post2) =>
+          post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+      },
+      searchPosts() {
+        return this.sortedPost.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+      }
+    },
+    watch: {
+    }
+  }
+  </script>
+  
+  <style>
+ 
+  .app__btns {
+    margin: 20px 0px;
+    display: flex;
+    justify-content: space-between;
+  }
+  
+  .observer {
+    height: 30px;
+  }
+  </style>
+  
